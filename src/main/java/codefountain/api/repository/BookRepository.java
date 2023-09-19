3b873b0d31a922b9599604e0dc5967bc53fe9087
syr67 <anikin56789@mail.ru>
2023-09-19 14:50:27 +0300

    add

---
+++ src/main/java/codefountain/api/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package codefountain.api.repository;
+
+import codefountain.api.domain.Book;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends PagingAndSortingRepository<Book, Long> {
+}
